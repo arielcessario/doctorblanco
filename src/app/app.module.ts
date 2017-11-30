ea33baabfc5c9ebf82f8f4405914c64c6e917ce5
Ariel <arielcessario@gmail.com>
2017-11-30 11:35:59 -0300

    Server y otros

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-
-import { AppComponent } from './app.component';
-
+import {AppComponent} from './app.component';
+import {Routing} from "./app.routes";
+import {NavComponent} from "./shared/nav/nav.component";
+import {CoreService} from "./core/core.service";
+import {AuthenticationService} from "./core/auth/authentication.service";
+import {Angular2FontawesomeModule} from 'angular2-fontawesome/angular2-fontawesome';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        NavComponent
+    ],
+    imports: [
+        BrowserModule,
+        Angular2FontawesomeModule,
+        Routing
+    ],
+    providers: [CoreService, AuthenticationService],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
